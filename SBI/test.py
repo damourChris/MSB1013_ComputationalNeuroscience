6d2b0e42078cfbd0f384c2c77db4d865976779b3
Chris <c.damour@student.maastrichtuniversity.nl>
2023-09-27 06:44:02 +0000

    Fixed syntax

--- SBI/test.py
+++ SBI/test.py
@@ -1,6 +1,9 @@
 def test_posterior(
         model = "models/posterior.pt", 
         test_data = "data/X.npy",
-        
-)
+    ):
     
+    test_results = ""
+    
+    
+    return test_results
