d1b9e9ce3cfc5aededf409f73741d4d4c5be5121
Zianor <zianor@zianor.de>
2023-10-04 17:49:44 +0200

    [BOLD] Bold works but on an offset

--- BOLD_Signal/BWM.py
+++ BOLD_Signal/BWM.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 # TODO: check whether default values are appropriate
-def balloon_windkessel(neural_activity, dt=0.1,
-                       tau=0.98, alpha=0.32,
-                       E_0=0.8, V_0=0.02,
-                       tau_s=0.8, tau_f=0.4, epsilon=0.5):
+def balloon_windkessel(f, dt=0.001,
+                       tau_mtt=2.0, alpha=0.32,
+                       E_0=0.4, V_0=4,
+                       tau_vs=4, epsilon=0.0463, rho_0=0.191, nu_0=126.3, TE=0.028):
     # alpha is 0.2 in Friston 2000
     # E_0 is 0.8 in Friston 2000 but also 0.34 in neurolib
     """
@@ -22,52 +22,70 @@ def balloon_windkessel(neural_activity, dt=0.1,
     
     :return: array-like, the BOLD response over time
     """
-    k1 = 7 * E_0
-    k2 = 2
-    k3 = 2 * E_0 - 0.2
-    n_time_points = len(neural_activity)
-    s = np.zeros(n_time_points)
-    f = np.ones(n_time_points)
-    v = np.ones(n_time_points)  # * V_0
-    q = np.ones(n_time_points)  # * V_0 * E_0
+    k1 = 4.3 * nu_0 * E_0 * TE
+    k2 = epsilon * rho_0 * E_0 * TE
+    k3 = 1 - epsilon
+    n_time_points = len(f)
+    v = np.zeros(n_time_points)
+    q = np.zeros(n_time_points)
     bold = np.zeros(n_time_points)
 
-    for t in range(1, n_time_points):
-        # equations same as in Friston 2000
-        f_out = v[t - 1] ** (1 / alpha)
-        ds = dt * (-s[t - 1] / tau_s + epsilon * neural_activity[t - 1] - (f[t-1] - 1) / tau_f)  # neural activity
-        df = dt * s[t - 1]  # vasodilatory signal
-        dv = dt * ((f[t - 1] - f_out) / tau)  # blood volume
-        # TODO: we get a RuntimeWarning (divide by zero) here, looks like v[t-1] is the reason
-        dq = dt * (f[t - 1] * ((1 - (1 - E_0) ** (1 / f[t - 1])) / E_0) - (f_out * q[t - 1]) / v[t - 1]) / tau  # deoxyhemoglobin
+    plt.figure()
+    plt.plot(f)
+    plt.show()
 
-        s[t] = s[t - 1] + ds
-        f[t] = max(f[t - 1] + df, 1e-120)
-        v[t] = v[t - 1] + dv
-        q[t] = q[t - 1] + dq
+    f_out = 0
+    curr_v = 0
+    curr_q = 0
+
+    for t in range(0, n_time_points):
+        E_f = 1 - (1 - E_0) ** (1 / f[t])
+
+        dv = dt * ((f[t] - f_out) / tau_mtt)  # blood volume
+
+        curr_v = curr_v + dv
+        f_out = curr_v ** (1 / alpha) + tau_vs * dv
+
+        dq = dt * (f[t] * (E_f / E_0) - f_out * (curr_q / curr_v) / tau_mtt)  # deoxyhemoglobin
+
+        v[t] = curr_v
+        curr_q = curr_q + dq
+        q[t] = curr_q
 
         bold[t] = V_0 * (k1 * (1.0 - q[t]) + k2 * (1.0 - q[t] / v[t]) + k3 * (1.0 - v[t]))
 
-    return bold
+    return bold, v, q
 
 
 # Example usage:
 import matplotlib.pyplot as plt
 
 # Create a simple block design neural activity
-neural_activity = np.zeros(1000)
-neural_activity[100:200] = 1
-neural_activity[400:500] = 1
-neural_activity[700:800] = 1
+t_sim = 40 # in s
+dt = 0.001
+input = np.zeros(int(t_sim/dt))
+input[int(10/dt):int(15/dt)] = 1
+X = np.ones(int(t_sim/dt))
+for t in range(int(t_sim/dt)):
+    X[t] = X[t-1] + dt * (input[t] - X[t-1] + 1)
 
 # Get the BOLD response
-bold_response = balloon_windkessel(neural_activity)
+bold, v, q = balloon_windkessel(X, dt=0.001)
+print(bold)
 
 # Plot the results
-plt.figure(figsize=(10, 5))
-plt.plot(neural_activity, label='Neural Activity')
-plt.plot(bold_response*100, label='BOLD Response (scaled by 100)')
-plt.xlabel('Time (s)')
+plt.figure(figsize=(10, 10))
+plt.subplot(6,1,1)
+plt.plot(input, label="stimulus")
+plt.subplot(6,1,2)
+plt.plot(X, label='Neural Activity')
+plt.subplot(6,1,3)
+plt.plot(bold, label='BOLD Response')
+plt.subplot(6,1,4)
+plt.plot(v, label='v')
+plt.subplot(6,1,5)
+plt.plot(q, label='q')
+plt.xlabel('Time (ms)')
 plt.ylabel('Amplitude')
 plt.legend()
 plt.show()
