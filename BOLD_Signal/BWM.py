96a4668875c13d8a002f7b5a019983e1b9a302ba
Zianor <zianor@zianor.de>
2023-10-05 16:26:45 +0200

    [BOLD] Fix bold simulation

--- BOLD_Signal/BWM.py
+++ BOLD_Signal/BWM.py
@@ -1,90 +1,129 @@
 import numpy as np
 
-# TODO: check whether default values are appropriate
-def balloon_windkessel(f, dt=0.001,
-                       tau_mtt=2.0, alpha=0.32,
-                       E_0=0.4, V_0=4,
-                       tau_vs=4, epsilon=0.0463, rho_0=0.191, nu_0=126.3, TE=0.028):
-    # alpha is 0.2 in Friston 2000
-    # E_0 is 0.8 in Friston 2000 but also 0.34 in neurolib
+
+def get_ballon_windkessel_params():
+    # TODO: check whether descriptions are correct
+    """
+    E_0: float, the resting oxygen extraction fraction, also called rho
+    V_0: float, the resting blood volume fraction
+    tau_vs: float, the time constant for the signal decay
+    tau_mtt: float, the hemodynamic transit time
+    alpha: float, the Grubb's exponent
+    """
+    return {
+        'c1': 0.6,
+        'c2': 1.5,
+        'c3': 0.6,
+        'tau_mtt': 2,
+        'tau_vs': 4,
+        'alpha': 0.32,
+        'E_0': 0.4,
+        'V_0': 4,
+        'eps': 0.0463,
+        'rho_0': .191,
+        'nu_0': 126.3,
+        'TE': 0.028
+    }
+
+
+def balloon_windkessel(neural_activity, dt=0.001):
     """
     Simulates the BOLD response using the Balloon-Windkessel model.
     
     :param neural_activity: array-like, the neural activity over time
     :param dt: float, the time step
-    :param tau: float, the hemodynamic transit time
-    :param alpha: float, the Grubb's exponent
-    :param E_0: float, the resting oxygen extraction fraction, also called rho
-    :param V_0: float, the resting blood volume fraction
-    :param tau_s: float, the time constant for the signal decay
-    :param tau_f: float, the time constant for the signal rise
-    :param epsilon: float, unknown parameter from Friston2000
     
     :return: array-like, the BOLD response over time
     """
-    k1 = 4.3 * nu_0 * E_0 * TE
-    k2 = epsilon * rho_0 * E_0 * TE
-    k3 = 1 - epsilon
-    n_time_points = len(f)
+    theta = get_ballon_windkessel_params()
+    k1 = 4.3 * theta['nu_0'] * theta['E_0'] * theta['TE']
+    k2 = theta['eps'] * theta['rho_0'] * theta['E_0'] * theta['TE']
+    k3 = 1 - theta['eps']
+    n_time_points = len(neural_activity)
+
     v = np.zeros(n_time_points)
     q = np.zeros(n_time_points)
     bold = np.zeros(n_time_points)
 
-    plt.figure()
-    plt.plot(f)
-    plt.show()
+    f = np.zeros(n_time_points)
+
+    theta = {'c1': 0.6, 'c2': 1.5, 'c3': 0.6,
+             'tau_mtt': 2, 'tau_vs': 4, 'alpha': 0.32, 'E_0': 0.4, 'V_0': 4, 'eps': 0.0463, 'rho_0': .191,
+             'nu_0': 126.3, 'TE': 0.028}
+
+    xinflow = 0
+    xvaso = 0
+    yinflow = 0
+    yvaso = 0
+
+    # neurovascular coupling, also performs low pass
+    for t in range(n_time_points):
+        xinflow = np.exp(xinflow)
+        yvaso = yvaso + dt * (neural_activity[t] - theta['c1'] * xvaso)  # vasoactive signal
+        df_a = theta['c2'] * xvaso - theta['c3'] * (xinflow - 1)  # inflow
+        yinflow = yinflow + dt * (df_a / xinflow)
+        xvaso = yvaso
+        xinflow = yinflow
+
+        f[t] = np.exp(yinflow)
 
     f_out = 0
-    curr_v = 0
-    curr_q = 0
+    curr_v = 1
+    curr_q = 1
 
-    for t in range(0, n_time_points):
-        E_f = 1 - (1 - E_0) ** (1 / f[t])
+    for t in range(n_time_points):
+        E_f = 1 - (1 - theta['E_0']) ** (1 / f[t])  # oxygen extraction fraction
 
-        dv = dt * ((f[t] - f_out) / tau_mtt)  # blood volume
+        dv = dt * ((f[t] - f_out) / theta['tau_mtt'])  # blood volume
+
+        dq = dt * ((f[t] * (E_f / theta['E_0']) - f_out * (curr_q / curr_v)) / theta['tau_mtt'])  # deoxyhemoglobin
+
+        f_out = curr_v ** (1 / theta['alpha']) + theta['tau_vs'] * dv
 
         curr_v = curr_v + dv
-        f_out = curr_v ** (1 / alpha) + tau_vs * dv
-
-        dq = dt * (f[t] * (E_f / E_0) - f_out * (curr_q / curr_v) / tau_mtt)  # deoxyhemoglobin
-
         v[t] = curr_v
         curr_q = curr_q + dq
         q[t] = curr_q
 
-        bold[t] = V_0 * (k1 * (1.0 - q[t]) + k2 * (1.0 - q[t] / v[t]) + k3 * (1.0 - v[t]))
+        bold[t] = theta['V_0'] * (k1 * (1.0 - q[t]) + k2 * (1.0 - q[t] / v[t]) + k3 * (1.0 - v[t]))
 
-    return bold, v, q
+    return bold, f, v, q
 
 
 # Example usage:
 import matplotlib.pyplot as plt
 
 # Create a simple block design neural activity
-t_sim = 40 # in s
+t_sim = 50  # in s
 dt = 0.001
-input = np.zeros(int(t_sim/dt))
-input[int(10/dt):int(15/dt)] = 1
-X = np.ones(int(t_sim/dt))
-for t in range(int(t_sim/dt)):
-    X[t] = X[t-1] + dt * (input[t] - X[t-1] + 1)
+U = np.zeros(int(t_sim / dt))
+U[int(10 / dt):int(15 / dt)] = 1
+x = 0
+X = np.ones(int(t_sim / dt))
+for t in range(int(t_sim / dt)):
+    x = x + dt * (U[t] - x)
+    X[t] = x
 
 # Get the BOLD response
-bold, v, q = balloon_windkessel(X, dt=0.001)
-print(bold)
+bold, f, v, q = balloon_windkessel(X, dt=0.001)
 
 # Plot the results
-plt.figure(figsize=(10, 10))
-plt.subplot(6,1,1)
-plt.plot(input, label="stimulus")
-plt.subplot(6,1,2)
-plt.plot(X, label='Neural Activity')
-plt.subplot(6,1,3)
-plt.plot(bold, label='BOLD Response')
-plt.subplot(6,1,4)
-plt.plot(v, label='v')
-plt.subplot(6,1,5)
-plt.plot(q, label='q')
+plt.figure(figsize=(5, 10))
+plt.subplot(6, 1, 1)
+plt.plot(U[6000:], lw=3)
+plt.title('Stimulus')
+plt.subplot(6, 1, 2)
+plt.plot(f[6000:])
+plt.title('Cerebral Blood Flow')
+plt.subplot(6, 1, 3)
+plt.plot(v[6000:], lw=3)
+plt.title('Cerebral Blood Volume')
+plt.subplot(6, 1, 4)
+plt.plot(q[6000:], lw=3)
+plt.title('Deoxyhemoglobin Content')
+plt.subplot(6, 1, 5)
+plt.plot(bold[6000:], lw=3)
+plt.title('BOLD Signal')
 plt.xlabel('Time (ms)')
 plt.ylabel('Amplitude')
 plt.legend()
