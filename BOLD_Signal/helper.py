[BOLD] Fix the beta calculation and add example with stimulation

BOLD_Signal/helper.py
@@ -1,6 +1,8 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 from BOLD_Signal.BWM import balloon_windkessel
+from BOLD_Signal.DMF_model import HRF
 
 
 def combine_inh_exc_abs_sum(Y, layers=4):
@@ -66,7 +68,8 @@ def downsample_neural_activity(neural_signal, original_sample_rate=1e-4, target_
     return resampled_signal
 
 
-def get_betas_from_neural_activity(Y, neural_activity_sampling_rate=1e-4, bold_sample_rate=0.001):
+def get_betas_from_neural_activity(Y, neural_activity_sampling_rate=1e-4, bold_sample_rate=0.001,
+                                   stim_start=10, stim_end=20):
     """For a single simulation
     TODO: extend to make it work on whole array
     """
@@ -83,24 +86,37 @@ def get_betas_from_neural_activity(Y, neural_activity_sampling_rate=1e-4, bold_s
     neural_activity = downsample_neural_activity(neural_activity)
 
     # bold responses for the layers
-    X = np.zeros(shape=(sampling_indices.shape[0], neural_activity.shape[1]))
+    bold_downsampled = np.zeros(shape=(sampling_indices.shape[0], neural_activity.shape[1]))
     bold_responses = np.zeros(shape=neural_activity.shape)
+
+    X = np.zeros((Y.shape[0], 4))
 
     for layer in range(neural_activity.shape[1]):
         bold, f, v, q = balloon_windkessel(neural_activity[:, layer])
         bold_responses[:, layer] = bold
-        bold = (bold - np.min(bold)) / (np.max(bold) - np.min(bold))  # normalize X between 0 and 1
 
         # sample bold with TR
         bold_response = bold[sampling_indices]
-        X[:, layer] = bold_response
+        bold_downsampled[:, layer] = bold_response
 
-    # down-sampled neural activity at fMRI scan acquisition points
-    Y = neural_activity[sampling_indices, :]
+        hrf = HRF(np.arange(0, 40, 1e-3))
+
+        # create condition vector for GLM analysis
+        condition = np.zeros((Y.shape[0]))
+        condition[int(stim_start/neural_activity_sampling_rate): int(stim_end/neural_activity_sampling_rate)] = 1
+
+        # predicted BOLD signal
+        currX = np.convolve(condition, hrf)[:len(condition)]
+        # scale between 0 and 1
+        currX = (currX - np.min(currX)) / (np.max(currX) - np.min(currX))
+
+        X[:, layer] = currX
+
+    # down-sampled X at fMRI scan acquisition points
+    X = X[::int(TR/neural_activity_sampling_rate), :]
+    plt.plot(X[:, 0])
 
     # scale betas to obtain original signal (Y = X*B)
-    B = (np.linalg.pinv(X @ X.T) @ X).T @ Y
-    # TODO: I just copied this from Kris' code but I don't understand why we do this
-    print(B)
+    B = (np.linalg.pinv(X @ X.T) @ X).T @ bold_downsampled
     B = B[0, :]
-    return B, X, bold_responses
+    return B, X, bold_downsampled

BOLD_Signal/stimulus_to_bold_example.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     t_sim = 50  # in s
     dt = 0.0001
     U = np.zeros(int(t_sim / dt))
-    U[int(10 / dt):int(15 / dt)] = 1
+    U[int(10 / dt):int(20 / dt)] = 1
     x = 0
     X = np.ones(int(t_sim / dt))
     for t in range(int(t_sim / dt)):
@@ -18,17 +18,20 @@ if __name__ == '__main__':
     Y = np.zeros(shape=(int(t_sim / dt), 8))
     for layer in range(8):
         Y[:, layer] = X * layer / 10  # scale differently per layer
-    Y = (Y - np.min(Y)) / (np.max(Y) - np.min(Y)) * 2 + 1  # normalize Y between -1 and 1
+    # # depends on what the simulations look like, goal is to make it a bit bigger
+    # Y = (Y - np.min(Y)) / (np.max(Y) - np.min(Y)) * 2 + 1  # normalize Y between -1 and 1
 
     B, X, bold_responses = get_betas_from_neural_activity(Y)
+
+    downsampledY = downsample_neural_activity(combine_inh_exc_only_exc(Y))
 
     colors = plt.cm.Spectral(np.linspace(0, 1, 4))
     plt.figure(figsize=(10, 6))
     plt.subplot(311)
     plt.gca().set_prop_cycle(plt.cycler('color', plt.cm.Spectral(np.linspace(0, 1, 4))))
     plt.title('Y')
-    plt.plot(combine_inh_exc_only_exc(Y))
-    plt.xlim([0, len(Y) - 1])
+    plt.plot(downsampledY)
+    plt.xlim([0, len(downsampledY) - 1])
     plt.subplot(312)
     plt.title('B')
     plt.bar(['L23', 'L4', 'L5', 'L6'], B, color=colors)
@@ -41,10 +44,9 @@ if __name__ == '__main__':
     plt.savefig('fig/signal_betas_example.pdf', bbox_inches='tight', transparent=True, dpi=300)
 
     plt.figure()
-    downsampledY = downsample_neural_activity(Y)
     for layer in range(4):
         plt.subplot(4, 1, layer + 1)
+        plt.plot(downsampledY[::int(2/0.001), layer], label="activity")
         plt.plot(bold_responses[:, layer], label="bold")
-        plt.plot(downsampledY[:, layer], label="activity")
         plt.legend()
     plt.savefig('fig/bold_neuronal_activity_example.pdf', bbox_inches='tight', transparent=True, dpi=300)