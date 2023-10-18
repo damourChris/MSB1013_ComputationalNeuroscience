76b3aba1f5643ec654feb9db1d87dc07aa988f69
Christopher Damour <84023113+Vilvis@users.noreply.github.com>
2023-10-18 15:19:36 +0200

    [SBI] Added Percentile Distrubtion Plot

--- SBI/utils.py
+++ SBI/utils.py
@@ -1,27 +1,43 @@
 import numpy as np
 import pylab as plt
 
+import math
 import torch
 
 
-def param_histogram(samples, labels=None, figsize=(10, 10)):
+def percentile_distribution(samples, labels=None, figsize=(10, 10), percentiles=[5,95], ylims=[-1000,1000]):
     """
     Create a histogram summary of parameters from samples.
     """
     num_samples, num_dims = samples.shape
-    plot_dims = ceil(sqrt(num_dims))
+    plot_dims = math.ceil(math.sqrt(num_dims))
 
     if (labels is None):
         labels = [r"$\theta_{}$".format(i) for i in range(num_dims)]
-
+    
+    plt.rcParams['figure.constrained_layout.use'] = True
     fig, ax = plt.subplots(plot_dims, plot_dims, figsize=figsize)
+    
     plt.suptitle(r'p($\theta | x$)', fontsize=20)
+    
+    p = np.linspace(percentiles[0], percentiles[1], 1000)
+    
     for i in range(plot_dims):    
-        for j in range(plot_dims):    
-            ax[i, j].hist(samples[:, i], bins=50, density=True, histtype="step", color="black")
-            ax[i, j].set_xlabel(labels[i])
-            ax[i, j].set_ylabel(labels[i])
-            ax[i, j].set_yticks([])
+        for j in range(plot_dims): 
+            indx = i + j*plot_dims 
+            if(indx < num_dims):
+                mat = np.matrix(samples[:, indx])
+                percentiles = np.percentile(samples[:, indx], p)
+                ax[j, i].plot(p, percentiles)
+                ax[j, i].plot(p, np.repeat(0, np.shape(p)))
+                ax[j, i].set_xlabel(labels[indx])
+                ax[j, i].set_ylabel(labels[indx])
+                
+                
+                ax[j, i].set_ylim(ylims)
+                # ax[j, i].set_yticks()
+            else:
+                ax[j, i].axis("off")
     return fig, ax
 
 
@@ -35,7 +51,7 @@ def pairplot(samples, labels=None, figsize=(10, 10)):
     if (labels is None):
         labels = [r"$\theta_{}$".format(i) for i in range(num_dims)]
 
-    fig, ax = plt.subplots(num_dims, num_dims, figsize=figsize)
+    fig, ax = plt.subplots(num_dims, num_dims, figsize=figsize, layout='constrained')
     plt.suptitle(r'p($\theta | x$)', fontsize=20)
     for i in range(num_dims):
         for j in range(num_dims):
